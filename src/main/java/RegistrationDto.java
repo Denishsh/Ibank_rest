35605b9971194260b4af647e3cc211d1cafd0a8d
test <test>
2022-02-16 15:08:16 +0300

    Update tests

--- src/main/java/RegistrationDto.java
+++
@@ -1,9 +0,0 @@
-import lombok.Data;
-
-@Data
-public class RegistrationDto {
-    private final String login;
-    private final String password;
-    private final String status;
-
-}
--- src/test/java/AuthTest.java
+++ src/test/java/AuthTest.java
@@ -1,7 +1,3 @@
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.filter.log.LogDetail;
-import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -9,37 +5,18 @@ import org.openqa.selenium.Keys;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static io.restassured.RestAssured.given;
 
 public class AuthTest {
-    private static RequestSpecification requestSpec = new RequestSpecBuilder()
-            .setBaseUri("http://localhost")
-            .setPort(9999)
-            .setAccept(ContentType.JSON)
-            .setContentType(ContentType.JSON)
-            .log(LogDetail.ALL)
-            .build();
 
     @BeforeAll
     static void setUpAll() {
-        given()
-                .spec(requestSpec) // указываем, какую спецификацию используем
-                .body(new RegistrationDto("vasya", "password", "active")) // передаём в теле объект, который будет преобразован в JSON
-                .when() // "когда"
-                .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
-                .then() // "тогда ожидаем"
-                .statusCode(200); // код 200 OK
+        DataGenerator.sendRequest(DataGenerator.Registration.getRegisteredUser());
     }
 
     @Test
     public void createBlockedUser() {
-        RegistrationDto user = new RegistrationDto("dima", "password", "blocked");
-        given().spec(requestSpec)
-                .body(user)
-                .when()
-                .post("/api/system/users")
-                .then()
-                .statusCode(200);
+        DataGenerator.sendRequest(DataGenerator.Registration.getUser("blocked"));
+        DataGenerator.RegistrationDto user = DataGenerator.Registration.getRegisteredUser();
 
         open("http://localhost:9999");
         $(".input__control[name=login]").doubleClick().sendKeys(Keys.BACK_SPACE);
@@ -55,7 +32,8 @@ public class AuthTest {
     @Test
     public void userExistsTest() {
         open("http://localhost:9999");
-        RegistrationDto user = new RegistrationDto("vasya", "password", "active");
+        DataGenerator.sendRequest(DataGenerator.Registration.getUser("active"));
+        DataGenerator.RegistrationDto user = DataGenerator.Registration.getRegisteredUser();
         $(".input__control[name=login]").doubleClick().sendKeys(Keys.BACK_SPACE);
         $(".input__control[name=login]").setValue(user.getLogin());
         $(".input__control[name=password]").doubleClick().sendKeys(Keys.BACK_SPACE);
@@ -67,9 +45,10 @@ public class AuthTest {
     @Test
     public void invalidLoginTest() {
         open("http://localhost:9999");
-        RegistrationDto user = new RegistrationDto("vas", "password", "active");
+        DataGenerator.sendRequest(DataGenerator.Registration.getUser("active"));
+        DataGenerator.RegistrationDto user = DataGenerator.Registration.getRegisteredUser();
         $(".input__control[name=login]").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $(".input__control[name=login]").setValue(user.getLogin());
+        $(".input__control[name=login]").setValue(user.getLogin() + "/");
         $(".input__control[name=password]").doubleClick().sendKeys(Keys.BACK_SPACE);
         $(".input__control[name=password]").setValue(user.getPassword());
         $("button").click();
@@ -81,11 +60,12 @@ public class AuthTest {
     @Test
     public void invalidPassTest() {
         open("http://localhost:9999");
-        RegistrationDto user = new RegistrationDto("vasya", "pass", "active");
+        DataGenerator.sendRequest(DataGenerator.Registration.getUser("active"));
+        DataGenerator.RegistrationDto user = DataGenerator.Registration.getRegisteredUser();
         $(".input__control[name=login]").doubleClick().sendKeys(Keys.BACK_SPACE);
         $(".input__control[name=login]").setValue(user.getLogin());
         $(".input__control[name=password]").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $(".input__control[name=password]").setValue(user.getPassword());
+        $(".input__control[name=password]").setValue(user.getPassword() + "/");
         $("button").click();
         $(".notification__title").shouldHave(text("Ошибка"));
         $(".notification__content")
